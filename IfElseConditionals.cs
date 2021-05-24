62f9f44addb15892bb57ff29a7b6979bb2b6b0cd
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-05-24 11:19:36 -0500

    If else conditional practice

---
+++ IfElseConditionals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_playground
+{
+    class IfElseConditionals
+    {
+        public double _value1;
+        public double _value2;
+        
+        public IfElseConditionals(double value1, double value2)
+        {
+            _value1 = value1;
+            _value2 = value2;
+        }
+        public void IfElseConditionalsActivelyWorkingOn()
+        {
+
+        }
+        public void IfElseConditionalsExamples()
+        {
+            if (_value1 > _value2)
+            {
+                Console.WriteLine($"{_value1} > {_value2}");
+            }
+            else if (_value1 < _value2)
+            {
+                Console.WriteLine($"{_value1} < {_value2}");
+            }
+            else if (_value1 == _value2)
+            {
+                Console.WriteLine($"{_value1} = {_value2}");
+            }
+            else
+            {
+                Console.WriteLine($"I don't know");
+            }
+
+            // "Short hand if" Ternary operator
+            Console.WriteLine((_value1 > _value2) ? $"{_value1} > {_value2}" : $"{_value1} < {_value2}"); // If values are same, outputs 2nd (false) expression
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace c_sharp_playground
     {
         static void Main()
         {
-            //Console.WriteLine("Enter Value1");
-            //double value1 = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine("Enter Value2");
-            //double value2 = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine();
-            Booleans Booleans = new Booleans();
-            Booleans.BooleanActivelyWorkingOn();
+            Console.WriteLine("Enter Value1");
+            double value1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Value2");
+            double value2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            IfElseConditionals IfElseConditionals = new IfElseConditionals(value1, value2);
+            IfElseConditionals.IfElseConditionalsActivelyWorkingOn();
 
             // var UserInput = new UserInput();
             // UserInput.UserInputProgram();
