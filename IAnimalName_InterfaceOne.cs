3b0bd907f19bc138580393233843bc69a9a71d3a
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-06-14 08:17:34 -0500

    Created two interface files and one implementation file that is supposed to implement from both. Only implementing one now

---
+++ IAnimalName_InterfaceOne.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_playground
+{
+    interface IAnimalName_InterfaceOne
+    {
+        public string AnimalName { get; set; }
+    }
+}
---
+++ InterfaceImplementationClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_playground
+{
+    class InterfaceImplementationClass : IAnimalSound_InterfaceTwo
+    {
+        public string _animalSound;
+
+        public InterfaceImplementationClass(string animalSound)
+        {
+            _animalSound = animalSound;
+        }
+        public void animalSound()
+        {
+            Console.WriteLine($"The animal says {_animalSound}");
+        }
+    }
+}
