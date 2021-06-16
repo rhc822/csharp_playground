563165d06249185851136a76cea3a08ef24ce315
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-06-16 09:00:47 -0500

    Created FileTest.cs to play around with CRUD for files

---
+++ FileTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace c_sharp_playground
+{
+    class FileTest
+    {
+        FileTestCreation()
+        {
+            string writeText = "Hello World!";
+            File.WriteAllText();
+        }
+        
+    }
+}
