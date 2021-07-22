679a73ab5b5fb7f10ad8b37030ca108f21e9808c
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-07-22 09:03:11 -0500

    Working on LINQ section of Microsoft Docs C# guide: https://docs.microsoft.com/en-us/dotnet/csharp/linq/query-expression-basics

--- Microsoft Docs - C sharp/LINQ.cs
+++ Microsoft Docs - C sharp/LINQ.cs
@@ -10,21 +10,53 @@ namespace c_sharp_playground
         
         public static void LinqExample()
         {
+            // Data source
             int[] scores = new int[] { 97, 92, 81, 60 };
 
-            IEnumerable<int> scoreQuery = 
+            // Query1
+            IEnumerable<int> highScoreQuery = 
                 from score in scores
                 where score > 80
                 orderby score ascending
                 select score;
 
-            foreach (int i in scoreQuery)
+            foreach (int i in highScoreQuery)
             {
 
-                Console.Write(i + " \n");
+                Console.Write($"{i} {i.GetType()} \n");
 
             }
 
+            // Query2
+            IEnumerable<string> scoreString =
+                from score in scores
+                where score > 80
+                orderby score descending
+                select $"The score is {score}";
+
+            foreach (string j in scoreString)
+            {
+
+                Console.Write($"{j} {j.GetType()}\n");
+
+            }
+
+            // Query3
+            int highScoreCount =
+                (from score in scores
+                 where score > 80
+                 select score)
+                 .Count();
+            Console.WriteLine(highScoreCount);
+
+            // OR Query3 re-written preferably as...
+
+            IEnumerable<int> highScoresQuery2 =
+                from score in scores
+                where score > 80
+                select score;
+            int scoreCount = highScoresQuery2.Count();
+            Console.WriteLine(scoreCount);
         } 
     }
 }
